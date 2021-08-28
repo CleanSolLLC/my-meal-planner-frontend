d4663912c0965bd276b6a1b941dec3ca6db28d48
mamark90 <mamark90@gmail.com>
2021-08-28 15:32:00 -0400

    added logic to fetch food_query and update DOM

--- js/scripts.js
+++ js/scripts.js
@@ -7,12 +7,33 @@
 // Use this file to add JavaScript to your project
 
 
-const endpoint = "http://localhost:3000/api/v1/foods"
+const endpoint = "http://localhost:3000/api/v1/food_queries"
 
 document.addEventListener("DOMContentLoaded", () => {
-  fetch(endpoint) 
-  	.then(response => response.json())
-  	.then(foods => { 
-  		console.log(foods)
-  })
+  getdashboardData()
 });
+
+function getdashboardData() {
+  fetch(endpoint) 
+    .then(response => response.json())
+    .then(food_queries => { 
+      loaddashboardData(food_queries)
+  });
+};
+
+function loaddashboardData(obj) {
+  obj.data.forEach(foodQuery => {
+    const foodQueryMarkup = 
+    `<div data-id=${foodQuery.id}>
+      <b>Q. ${foodQuery.attributes.search}</b>
+      <p><b>A.</b> ${foodQuery.attributes.response}</p>
+      <button data-id=${foodQuery.id}>delete</button>
+    <div>
+    <br></br>`;
+    document.querySelector('#food-questions').innerHTML += foodQueryMarkup  
+  });
+
+  if (document.querySelector('#food-questions').innerText === "") {
+    document.querySelector('#food-questions').innerText = "No Data Available"
+  };
+}
