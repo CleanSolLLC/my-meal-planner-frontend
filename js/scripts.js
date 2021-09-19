e8f955edb50f651bec39e430e8a52f07102c6425
mamark90 <mamark90@gmail.com>
2021-09-18 22:29:25 -0400

    implement post fetch for recipes

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 
 const endpoint = "http://localhost:3000/api/v1/food_queries";
-const imagesPath = "https://spoonacular.com/recipeImages/";
 const recipeEndpoint = "http://localhost:3000/api/v1/recipes";
+const imagesPath = "https://spoonacular.com/recipeImages/";
 
 const btnArry = document.getElementsByClassName("delete-button");
 
@@ -94,8 +94,8 @@ function initiateFoodQuery(e) {
 
 function deleteFoodQuery(e) {
   e.preventDefault()
-  data = e.target.attributes[3].value
-  url = endpoint + '/' + data
+  let data = e.target.attributes[3].value
+  let url = endpoint + '/' + data
 
   fetch(url, {
     method: 'DELETE', 
@@ -115,16 +115,121 @@ function deleteFoodQuery(e) {
      alert(errors);
    });
 
-  }
+}
+  
+
+function getRecipeInformation() {
+  fetch(recipeEndpoint, {
+     headers: {
+       'Content-Type': 'application/json',
+       'Access-Control-Allow-Origin': '*'
+     },  
+
+    })
+    .then(response => response.json())
+    .then(recipeData => { 
+
+      if (recipeData.data.length === 0) {
+        return document.querySelector(".recipe-container").innerText = "No Recipes Selected"
+      } else {
+        printRecipeCards(recipeData)  
+      }
+  })
+    .catch((errors) => {
+     alert(errors)
+   });
+};
+
+function printRecipeCards(obj) {
+  // COL-LG-8
+  //   ROW
+  //     COL-LG-6
+  //         CARD MB-4
+  //         CARD MB-4
+  //create a card; 2 columns of cards will append to <div class="col-lg-6"></div> il maxColCnt is < 2 or create a new div and class for col-lg-6 and append to that 
+
+
+  let arry = obj.data
+
+  let container = document.querySelector("#recipe-cards")
+  let recipeMarkup = ''
+
+
+  arry.forEach(function(recipe, i) {
+    if(i===0) {
+      recipeMarkup += `<div class="row">`
+    }
+    let recipeImage = imagesPath + recipe.attributes.image;
+    
+     recipeMarkup += 
+    `<div class="col-lg-6">
+        <div class="card mb-4">
+            <div class="card-body">
+                <img class="card-img-top" src=${recipeImage} style="width:285px;height:285px;"/>
+                <b>Title: ${recipe.attributes.title}</b>
+                <p><b>Ready In:</b> ${recipe.attributes.readyInMinutes}</p>
+                <p><b>Servings:</b> ${recipe.attributes.servings}</p>
+                <p><a href="${recipe.attributes.sourceUrl}"target="_blank">${recipe.attributes.sourceUrl}</a></p>
+                <button class="delete-recipe-button" type="button" value="Delete" data-id=${recipe.id}>Delete</button>
+            </div>
+        </div>
+      </div>`;
+
+    if(i!==0 && i%2 !== 0){
+    // add end of row ,and start new row on every 5 elements
+    recipeMarkup += `</div><div class="row">`
+    } 
+
+  });
+
+  recipeMarkup += `</div>`;
+  container.innerHTML += recipeMarkup;
+
+    const btnArry = document.getElementsByClassName("delete-recipe-button")
+    for (var i = 0; i < btnArry.length; i++) {
+        var self = btnArry[i];
+        self.addEventListener('click', function (e) {  
+        if (e.type === "click") {
+          deleteRecipe(e)
+        }
+    });
+  }  
+
+}
+
+
+function deleteRecipe(e) {
+  e.preventDefault()
+  let data = e.target.attributes[3].value
+  let url = recipeEndpoint + '/' + data
+
+  fetch(url, {
+    method: 'DELETE', 
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: null
+  })
+  .then(response => response.json())
+  .then(json => {
+    //return json
+    window.location.reload()
+
+  })
+
+   .catch((errors) => {
+     alert(errors);
+   });
+
+}
 
 document.querySelector("#button-recipe-search").addEventListener("click", function(e) {
-  recipeCriteria = getFoodListValues(e);
-  getRecipeInformation();
-
+  recipeCriteria = getRecipeListValues(e);
+  initiateRecipeSearch(recipeCriteria) ;
 });  
 
 
-function getFoodListValues(e) {
+function getRecipeListValues(e) {
   e.preventDefault()
 
 
@@ -144,143 +249,64 @@ function getFoodListValues(e) {
 
     }
 
-
     for (const property in obj) {
       if (obj[property] !== '') {
-        newObj[property] = obj[property]}
+        newObj[property] = obj[property]
       }
+    }
 
     return newObj;
-  
+  }
+
   //document.querySelector(".form-control").value;
-}
-
-// function initiateRecipeSearch(recipeCriteria) {
-
-//   //let url = "https://webknox-recipes.p.rapidapi.com/recipes/search"
-
-//   //url += '?' + ( new URLSearchParams( recipeCriteria ) ).toString();
-
-//   fetch(recipeEndpoint, {
-//   "method": "GET",
-//   "headers": {
-//     "x-rapidapi-host": config["RAPID-API-HOST"],
-//     "x-rapidapi-key":  config["RAPID-API-KEY"]
-//   }
-// })
-//   .then(response => response.json())
-//   .then(data => {
-
-//     createRecipeCards(data)
-
-//   })
-
-//   .catch(err => {
-//   console.error(err);
-// });
-
-// }
 
 
-// function getRecipeInformation() {
-//   fetch(recipeEndpoint) 
+function initiateRecipeSearch(recipeCriteria) {
 
-//     .then(response => response.json())
-//     .then(recipeData => { 
+  let url = "https://webknox-recipes.p.rapidapi.com/recipes/search"
 
-//       if (recipeData.data.length === 0) {
-//         return document.querySelector(".recipe-container").innerText = "No Recipes Selected"
-//       } else {
-//         createRecipeCards(recipeData)  
-//       }
-//   })
-//     .catch((errors) => {
-//      alert(errors)
-//    });
-// };
+  url += '?' + ( new URLSearchParams( recipeCriteria ) ).toString();
 
-
-function getRecipeInformation() {
-  fetch(recipeEndpoint, {
-     headers: {
-       'Content-Type': 'application/json',
-       'Access-Control-Allow-Origin': '*'
-     },  
-
-    })
-    .then(response => response.json())
-    .then(recipeData => { 
-
-      if (recipeData.data.length === 0) {
-        return document.querySelector(".recipe-container").innerText = "No Recipes Selected"
-      } else {
-        createRecipeCards(recipeData)  
-      }
+  fetch(url, {
+  "method": "GET",
+  "headers": {
+    "x-rapidapi-host": config["RAPID-API-HOST"],
+    "x-rapidapi-key":  config["RAPID-API-KEY"]
+  }
+})
+  .then(response => response.json())
+  .then(data => {
+    postRecipeData(data)
   })
-    .catch((errors) => {
-     alert(errors)
-   });
-};
-
-function createRecipeCards(data) {
-  // COL-LG-8
-  //   ROW
-  //     COL-LG-6
-  //         CARD MB-4
-  //         CARD MB-4
-  //create a card; 2 columns of cards will append to <div class="col-lg-6"></div> il maxColCnt is < 2 or create a new div and class for col-lg-6 and append to that 
-
-
-  let arry = data.data
-  //debugger;
-
-  let container = document.querySelector("#recipe-cards")
-  let recipeMarkup = ''
-
-
-  arry.forEach(function(recipe, i) {
-    if(i===0) {
-      recipeMarkup += `<div class="row">`
-    }
-    let recipeImage = imagesPath + recipe.attributes.image;
-    
-     recipeMarkup += 
-    `<div class="col-lg-6">
-        <div class="card mb-4">
-            <div class="card-body">
-                <img class="card-img-top" src=${recipeImage} />
-                <b>Title: ${recipe.attributes.title}</b>
-                <p><b>Ready In:</b> ${recipe.attributes.ready_in_minutes}</p>
-                <p><b>Servings:</b> ${recipe.attributes.servings}</p>
-                <p><a href="${recipe.attributes.source_url}"target="_blank">${recipe.attributes.source_url}</a></p>
-                <button class="save-button" type="button" value="Save" data-id=${recipe.attributes.id}>Save</button>
-                <button class="remove-button" type="button" value="Remove" data-id=${recipe.attributes.id}>Remove</button>
-            </div>
-        </div>
-      </div>`;
-
-    if(i!==0 && i%2 !== 0){
-    // add end of row ,and start new row on every 5 elements
-    recipeMarkup += `</div><div class="row">`
-    } 
-
-  });
-
-  recipeMarkup += `</div>`;
-  container.innerHTML += recipeMarkup;
+  .catch(err => {
+  console.error(err);
+});
 
 }
 
-//createRecipeCards(arry);
 
 
+function postRecipeData(data) {
+  // document.querySelector('.medium').innerText = ""
+  // const search = document.querySelector(".form-control").value;
+  // const data = {search: search};
 
+  fetch("http://localhost:3000/api/v1/recipes", {
+    method: 'POST', 
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+      window.location.reload()
+  // .then(res => res.text())          // convert to plain text
+  // .then(text => console.log(text))  // then log it out
 
+   .catch((errors) => {
+     alert(errors);
+   });
 
-      
-
-
-
+}
 
 
 
